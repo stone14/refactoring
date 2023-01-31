7aaa1df47da51bccdf00f0db777bd8e4b636d54c
stone14 <junil.shin@gmail.com>
2023-01-31 13:48:14 +0900

    컨트롤러 분리

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.render('home/index')
-})
 
-router.get('/login', (req, res) => {
-    res.render('home/login')
-})
+const ctrl = require('./home.ctrl')
+
+router.get('/',  ctrl.home)
+router.get('/login', ctrl.login)
 
 module.exports = router
