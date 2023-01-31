57ee07b53d0ff6a876246a6f1920483b0a0e3859
stone14 <52732440+stone14@users.noreply.github.com>
2023-01-31 12:51:15 +0900

    route 분리

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set("views", "./views")
+
+
+
+const home = require('./routes/home')
+app.use('/', home)
+// app.use('/login', home)
+app.listen(port, function () {
+    console.log('Start Server')
+})
---
+++ routes/home/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.render('home/index')
+})
+
+router.get('/login', (req, res) => {
+    res.render('home/login')
+})
+
+module.exports = router
